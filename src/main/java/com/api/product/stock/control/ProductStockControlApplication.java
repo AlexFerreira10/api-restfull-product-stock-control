6c34b8c55d4a258c3f0d8b82316f813ff4b2d868
AlexFerreira10 <correaferreiraalex92@email.com>
2024-07-31 18:15:29 -0300

    Initial commit

---
+++ src/main/java/com/api/product/stock/control/ProductStockControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.product.stock.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/product/stock/control/ProductStockControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.product.stock.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStockControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

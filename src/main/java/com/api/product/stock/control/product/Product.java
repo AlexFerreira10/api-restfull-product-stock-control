0a9ee5b150756cda886b740e9f694fae13356f4e
AlexFerreira10 <correaferreiraalex92@email.com>
2024-07-31 18:55:13 -0300

    Product entity mapping

---
+++ src/main/java/com/api/product/stock/control/product/Product.java
@@ -0,0 +1,51 @@
+package com.api.product.stock.control.product;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "tb_product")
+@Entity(name = "Product")
+public class Product {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantify")
+    private Integer quantify;
+
+    @Column(name = "validity")
+    private LocalDate validity;
+
+    @Column(name = "price")
+    private Double price;
+
+    public Product(RegisterProductDto dados) {
+        this.name = dados.name();
+        this.quantify = dados.quantify();
+        this.validity = dados.validity();
+        this.price = dados.price();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantify=" + quantify +
+                ", validity=" + validity +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/com/api/product/stock/control/product/RegisterProductDto.java
@@ -0,0 +1,22 @@
+package com.api.product.stock.control.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record RegisterProductDto(
+
+        @NotBlank(message = "The name field can't be empty")
+        String name,
+
+        @NotNull(message = "The quantify field can't be empty")
+        Integer quantify,
+
+        @NotNull(message = "The validity field can't be empty")
+        LocalDate validity,
+
+        @NotNull(message = "The price field can't be empty")
+        Double price
+) {
+}

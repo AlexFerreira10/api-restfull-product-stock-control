4f2b72f3a1f9b904ba7f35ec553c7bdb57dfb3f8
AlexFerreira10 <correaferreiraalex92@email.com>
2024-08-04 15:41:16 -0300

    User entity mapping and created the V4 migration with the user table

---
+++ src/main/resources/db/migration/V4__CREATE_TABLE-TB_USER.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tb_user (
+                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            login VARCHAR(255) NOT NULL,
+                            password varchar(255) NOT NULL
+);
